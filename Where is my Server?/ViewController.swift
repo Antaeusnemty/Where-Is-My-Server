19dd77c607fa4c7bcb257d997ea18ef7caa685c6
Antaeusnemty <gamingemail007@gmail.com>
2016-04-07 20:04:37 -0400

    staging for push

--- Where is my Server?/ViewController.swift
+++ Where is my Server?/ViewController.swift
@@ -55,7 +55,6 @@ class ViewController: UIViewController {
         longPressRecognizer.addTarget(self, action: "longPressFunction")
         lowerDisplay.addGestureRecognizer(longPressRecognizer)
         lowerDisplay.userInteractionEnabled = true
-       
         swipeRecognizer.addTarget(self, action: "swipeFunction")
         downSwipeRecognizer.addTarget(self, action: "swipeFunction")
         upSwipeRecognizer.addTarget(self, action: "swipeFunction")
@@ -65,70 +64,48 @@ class ViewController: UIViewController {
         upSwipeRecognizer.direction = UISwipeGestureRecognizerDirection.Up
         leftSwipeRecognizer.direction = UISwipeGestureRecognizerDirection.Left
         rightSwipeRecognizer.direction = UISwipeGestureRecognizerDirection.Right
-        
         lowerDisplay.addGestureRecognizer(swipeRecognizer)
         lowerDisplay.addGestureRecognizer(downSwipeRecognizer)
         lowerDisplay.addGestureRecognizer(upSwipeRecognizer)
         lowerDisplay.addGestureRecognizer(leftSwipeRecognizer)
         lowerDisplay.addGestureRecognizer(rightSwipeRecognizer)
         lowerDisplay.userInteractionEnabled = true
-        
-        
-        
         }//end of view did load
     
-// I need the ability to grab a string out of an array in all 3 of these functions.*** longPress & swipeFunction should grab a string from lowerArray ***
     @IBAction func button(sender: AnyObject)
     {
-        //display.text = selected ** moved this below actions so it will display changed text on first click
-        
-        //change the .text of the label(lowerDisplay)
         lowerDisplay.text = "Squeeze or Swipe Here! If your really hangery!"
-        musicPlayer2!.play()//play exclamation of namesake
+        musicPlayer2!.play()
+        
         //generate a random number (within the number of how many strings I have)(could always add more and update range)
         //set it equal to a new variable
-        //use the new variable as indexOf() or other index config stuff in order to grab a random string out of array
+        //use the new variable in order to grab a random string out of array
         let randomIndex = Int(arc4random_uniform(UInt32(array.count))) //Source: http://stackoverflow.com/questions/24003191/pick-a-random-element-from-an-array
-        //print(randomIndex)//prints Int value (random Int within range of array)
-        //print(array[randomIndex])//prints string
+        
         selected = array[randomIndex]
-        //print(selected)
         print("button ran")
-        
         display.text = selected
-        
         var randomNumber = random() / (array.count*100000000)
         print(randomNumber)
         print(array[randomNumber])
     }
     func longPressFunction()
-    { print("longPress ran")
-        display.text = selected
+    {
         lowerDisplay.text = "Squeeze or Swipe Here! If your really hangery!"
-        musicPlayer!.play()//play exclamation of namesake
-        //generate a random number (within the number of how many strings I have)(could always add more and update range)
-        //set it equal to a new variable
-        //use the new variable in order to grab a random string out of array
+        musicPlayer!.play()
         let randomIndex = Int(arc4random_uniform(UInt32(lowerArray.count)))
-        print(randomIndex)//prints Int value (random Int within range of array)
-        print(lowerArray[randomIndex])//prints string
         selected = lowerArray[randomIndex]
-        print(selected)
         display.text = selected
+        print("longPress ran")
     }
     func swipeFunction()
-    { print("swipeFunction ran")
-        display.text = selected
+    {
         lowerDisplay.text = "Squeeze or Swipe Here! If your really hangery!"
-        musicPlayer!.play()//play exclamation of namesake
-        //generate a random number (within the number of how many strings I have)(could always add more and update range)
-        //set it equal to a new variable
-        //use the new variable as indexOf() or other index config stuff in order to grab a random string out of array
+        musicPlayer!.play()
         let randomIndex = Int(arc4random_uniform(UInt32(lowerArray.count)))
         print(randomIndex)//prints Int value (random Int within range of array)
         print(lowerArray[randomIndex])//prints string
         selected = lowerArray[randomIndex]
-        print(selected)
         display.text = selected
     
     }
